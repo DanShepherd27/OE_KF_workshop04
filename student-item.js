89867de7e4c6c55c2e1c26f8087059dac53f3333
DanShepherd27 <jdani20001027@gmail.com>
2023-04-18 18:42:00 +0200

    Fix casing issues

--- student-item.js
+++ student-item.js
@@ -1,35 +1,44 @@
 export default {
-    props: ['id', 'image', 'name', 'isActive', 'birthYear', 'connections', 'completedCredits', 'activeSemesterCount' ],
+  props: [
+    "id",
+    "image",
+    "name",
+    "is-active",
+    "birth-year",
+    "connections",
+    "completed-credits",
+    "active-semester-count",
+  ],
 
-    methods: {
-        async deleteStudent() {
-            this.$parent.students.splice(this.$parent.students.indexOf(this.$parent.students.find( s => s.id === this.id)), 1);
-            // API hívás
-            // await fetch(`https://practiceapi.nikprog.hu/Student/${this.id}`, {
-            //     method: "delete",
-            // });
-        }
+  methods: {
+    async deleteStudent() {
+      this.$parent.students.splice(this.index, 1);
+      // API hívás
+      // await fetch(`https://practiceapi.nikprog.hu/Student/${this.id}`, {
+      //     method: "delete",
+      // });
+    },
+  },
+
+  computed: {
+    index() {
+      return this.$parent.students.findIndex((s) => s.id === this.id);
     },
 
-    computed: {
-        index() {
-            return this.$parent.students.indexOf(this.$parent.students.find( s => s.id === this.id));
-        }, 
-
-        avatarBorder() {
-            return this.isActive ? 'border-success' : 'border-danger';
-        },
-
-        nameColor() {
-            return this.isActive ? 'text-success' : 'text-danger';
-        }
+    avatarBorder() {
+      return this.isActive ? "border-success" : "border-danger";
     },
 
-    template: `
+    nameColor() {
+      return this.isActive ? "text-success" : "text-danger";
+    },
+  },
+
+  template: `
     <tr class="m-3"
     >
         <td>{{ index }}</td>
-        <td><img v-bind:src="image" style="width: 30px; height: 30px; border-radius: 30px;" class="border " v-bind:class="avatarBorder"></td>
+        <td><img v-bind:src="image" style="width: 30px; height: 30px; border-radius: 30px;" class="border " :class="avatarBorder"></td>
         <td v-bind:class="nameColor" >{{ name }}</td>
         <td>{{ id }}</td>
         <td>{{ isActive }}</td>
@@ -41,11 +50,11 @@ export default {
             <button
                 type="button"
                 class="btn btn-danger m-2"
-                @click="deleteStudent()"
+                @click="deleteStudent"
             >
                 Delete
             </button>
         </td>
     </tr>
-    `
-}
+    `,
+};
